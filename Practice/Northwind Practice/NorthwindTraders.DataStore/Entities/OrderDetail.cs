54815386a6bb5028a1110a42eac6908bcf48447b
Dan Gilleland <dgilleland@nait.ca>
2020-09-17 16:19:06 -0600

    Finished 2 tables and related DAL for NorthwindTraders.DataStore project

---
+++ Practice/Northwind Practice/NorthwindTraders.DataStore/Entities/OrderDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindTraders.DataStore.Entities
+{
+    [Table("Order Details")]
+    public class OrderDetail
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int OrderID { get; set; }
+        [Key, Column(Order = 2)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ProductID { get; set; }
+        [Column(TypeName = "money")]
+        public decimal UnitPrice { get; set; }
+        public short Quantity { get; set; }
+        public float Discount { get; set; }
+
+        // TODO: Navigation Properties: Product, Order
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ Practice/Northwind Practice/NorthwindTraders.DataStore/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using NorthwindTraders.DataStore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindTraders.DataStore
+{
+    public class NorthwindContext : DbContext
+    {
+        public NorthwindContext() : base("name=NW2018")
+        { }
+
+        #region "Virtual" Tables
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        // TODO: Add remaining tables for each entity in this class library
+        #endregion
+    }
+}

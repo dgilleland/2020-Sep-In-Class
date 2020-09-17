7d403cc33d0e12861de61ca4ca19fbbe0069c79b
Dan Gilleland <dgilleland@nait.ca>
2020-09-17 16:40:37 -0600

    Re #1: Added the CRUD controller and started the CRUD page for Products

---
+++ Practice/Northwind Practice/WebApp/CRUDReview/ManageProducts.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ManageProducts.aspx.cs" Inherits="WebApp.CRUDReview.ManageProducts" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <h1 class="page-header">Manage Products</h1>
+        <asp:DropDownList ID="CurrentProducts" runat="server"></asp:DropDownList>
+    </div>
+</asp:Content>
---
+++ Practice/Northwind Practice/WebApp/CRUDReview/ManageProducts.aspx.cs
@@ -0,0 +1,29 @@
+﻿using NorthwindTraders.BLL.CRUD;
+using NorthwindTraders.DataStore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp.CRUDReview
+{
+    public partial class ManageProducts : System.Web.UI.Page
+    {
+        // TODO: Create a CRUD page to edit/add/remove a product
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                // Populate the Products DropDown
+                var controller = new ProductController();
+                var data = controller.ListAllProducts();
+                CurrentProducts.DataSource = data;
+                CurrentProducts.DataTextField = nameof(Product.ProductName);
+                CurrentProducts.DataValueField = nameof(Product.ProductID);
+                CurrentProducts.DataBind();
+            }
+        }
+    }
+}

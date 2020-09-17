6ab65bef43466cfcf728f437de96515ee3662b75
Dan Gilleland <dgilleland@nait.ca>
2020-09-17 14:07:58 -0600

    ESP6-Recap - Added extra classes

---
+++ src/EF6-Recap/WestWindConsole/Entities/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace WestWindConsole.Entities
+{
+    public class Customer
+    {
+        [Key, Required, StringLength(5, MinimumLength = 5)]
+        public string CustomerID { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string ContactTitle { get; set; }
+        public string ContactEmail { get; set; }
+        public int AddressID { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+
+        #region Navigation Properties
+        public virtual ICollection<Order> Orders { get; set; } = new HashSet<Order>();
+        #endregion
+    }
+}
---
+++ src/EF6-Recap/WestWindConsole/Entities/Order.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace WestWindConsole.Entities
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }
+        public int SalesRepID { get; set; }
+        [Required, StringLength(5, MinimumLength = 5)]
+        public string CustomerID { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? PaymentDueDate { get; set; }
+        public decimal? Freight { get; set; }
+        public bool Shipped { get; set; }
+        public string ShipName { get; set; }
+        public int? ShipAddressID { get; set; }
+        public string Comments { get; set; }
+
+        #region Navigation Properties
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new HashSet<OrderDetail>();
+        public virtual Customer Customer { get; set; }
+        #endregion
+    }
+}

2c64689cf95cd004cb3fc77eb17c598044603b99
Dan Gilleland <dgilleland@nait.ca>
2020-11-02 13:19:41 -0700

    Added GetPlaylistCounts() method

--- Practice/Chinook Tunes/ChinookTunes/BLL/CustomerController.cs
+++ Practice/Chinook Tunes/ChinookTunes/BLL/CustomerController.cs
@@ -24,5 +24,29 @@ namespace ChinookTunes.BLL
                 return result.ToList();
             }
         }
+
+        public PlaylistCounts GetPlaylistCounts(int customerId)
+        {
+            using(var context = new ChinookContext()) // context is our virtual database
+            {
+                // this is the only Linq Query Syntax statement I need
+                var allTracks = from sale in context.InvoiceLines // Each InvoiceLine is for a single Track purchase
+                                where sale.Invoice.CustomerId == customerId
+                                select sale;
+
+                // I use Linq Method Syntax to get specific Albumn/Artist info for counting
+                var allAlbums = allTracks.Select(t => t.Track.Album.AlbumId) // to select only AlbumIds
+                                .Distinct().Count(); // .Distinct() to get rid of duplicates
+                var allArtists = allTracks.Select(t => t.Track.Album.Artist.ArtistId) // to select only ArtistIds
+                                .Distinct().Count(); // .Distinct() to get rid of duplicates
+                var result = new PlaylistCounts
+                {
+                    AllTracks = allTracks.Count(),
+                    Albums = allAlbums,
+                    Artists = allArtists
+                };
+                return result;
+            }
+        }
     }
 }
---
+++ Practice/Chinook Tunes/ChinookTunes/ViewModels/PlaylistCounts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinookTunes.ViewModels
+{
+    public class PlaylistCounts
+    {
+        public int AllTracks { get; set; }
+        public int Albums { get; set; }
+        public int Artists { get; set; }
+    }
+}

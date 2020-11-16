a1e905f4d2e151ff39deef5752caaaad80fccb5c
Dan Gilleland <dgilleland@nait.ca>
2020-11-16 13:28:14 -0700

    Demo Security Project started.

---
+++ Practice/StateRegistration/GeorgiaVoterRegistration/BLL/DominionController.cs
@@ -0,0 +1,24 @@
+﻿using GeorgiaVoterRegistration.DAL;
+using GeorgiaVoterRegistration.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeorgiaVoterRegistration.BLL
+{
+    [DataObject]
+    public class DominionController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Voter> ListAllVoters()
+        {
+            using(var context = new ElectionContext())
+            {
+                return context.Voters.ToList();
+            }
+        }
+    }
+}
---
+++ Practice/StateRegistration/GeorgiaVoterRegistration/DAL/ElectionContext.cs
@@ -0,0 +1,21 @@
+﻿using GeorgiaVoterRegistration.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeorgiaVoterRegistration.DAL
+{
+    public class ElectionContext : DbContext
+    {
+        public ElectionContext()
+            : base("name=GAdb")
+        {
+
+        }
+
+        public DbSet<Voter> Voters { get; set; }
+    }
+}
